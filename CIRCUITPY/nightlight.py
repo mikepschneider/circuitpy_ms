831af7f39cdd12dd8c97a02497afe3c48b54ef27
Michael Schneider <michael@dronebase.com>
2018-03-01 00:07:04 -0800

    .

--- CIRCUITPY/nightlight.py
+++ CIRCUITPY/nightlight.py
@@ -1,35 +1,53 @@
-import board
-import digitalio
+# import board
+# import digitalio
+# from digitalio import DigitalInOut, Direction, Pull
+# import neopixel
+# import touchio
 from nonblockingtimer import NonBlockingTimer
-from digitalio import DigitalInOut, Direction, Pull
-import neopixel
-import touchio
 
-RED = 0x100000 # (0x10, 0, 0) also works
-YELLOW=(0x10, 0x10, 0)
-GREEN = (0, 0x10, 0)
-AQUA = (0, 0x10, 0x10)
-BLUE = (0, 0, 0x10)
-PURPLE = (0x10, 0, 0x10)
-BLACK = (0, 0, 0)
-#
-COLORS = [RED, YELLOW, GREEN, AQUA, BLUE, PURPLE, BLACK]
-#
+from simpleio import map_range
+from analogio import AnalogIn
+import board
+import neopixel
+import time
+ 
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write=0, brightness=.05)
+pixels.fill((0,0,0))
+pixels.show()
+ 
+analogin = AnalogIn(board.LIGHT)
+ 
+
 
 class NightLight(NonBlockingTimer):
     def __init__(self):
         super(NightLight, self).__init__()
-        self.led = digitalio.DigitalInOut(board.D13)
-        self.led.direction = digitalio.Direction.OUTPUT
-        self.value = True
+        # self.led = digitalio.DigitalInOut(board.D13)
+        # self.led.direction = digitalio.Direction.OUTPUT
+        # self.value = True
 
     def stop(self):
         self.led.value = False
 
     def next(self):
         if (super(NightLight,   self).next()):
-            self.led.value = not (self.led.value)
-            if (self.value):
-                super(NightLight, self).set_interval(0.1)
-            else:
-                super(NightLight, self).set_interval(0.05)
+
+            #light value remaped to pixel position
+            peak = map_range(analogin.value, 2000, 62000, 9, 0)
+            print(analogin.value)
+            print(int(peak))
+         
+            for i in range(0, 9, 1):
+                 if i <= peak:
+                     pixels[i] = (255, 50, 0)
+                 else:
+                     pixels[i] = (0,0,0)
+            pixels.show()
+         
+            time.sleep(0.01)
+
+            # self.led.value = not (self.led.value)
+            # if (self.value):
+            #     super(NightLight, self).set_interval(0.1)
+            # else:
+            #     super(NightLight, self).set_interval(0.05)
--- install_osx.sh
+++ install_osx.sh
@@ -1,3 +1,3 @@
 rsync             --verbose --update CIRCUITPY/code.py  /Volumes/CIRCUITPY/
 rsync --recursive --verbose --update CIRCUITPY/*.mpy    /Volumes/CIRCUITPY/
-# rsync --recursive --verbose --update CIRCUITPY/lib/     /Volumes/CIRCUITPY/lib
+rsync --recursive --verbose --update CIRCUITPY/lib/     /Volumes/CIRCUITPY/lib
